48a5737a7190cc5969ebe9fc2c4120e796c3a7d9
kjl-macbook <keith.ja.lee@gmail.com>
2024-01-06 15:52:28 -0800

    sphere coordinate maker

--- src/init.jl
+++ src/init.jl
@@ -11,6 +11,12 @@ Y = [[sphevaluate(l, m; resolution = resolution) for m = -l:l] for l = 0:l_max]
 thetarange = range(0, pi, resolution+1)
 phirange = range(0, 2pi, 2resolution+1)
 
+make_xsphere(thetarange::AbstractRange, phirange::AbstractRange) = [cos(phi) * sin(theta) for theta in thetarange, phi in phirange]
+make_ysphere(thetarange::AbstractRange, phirange::AbstractRange) = [sin(phi) * sin(theta) for theta in thetarange, phi in phirange]
+make_zsphere(thetarange::AbstractRange, phirange::AbstractRange) = [cos(theta) for theta in thetarange, phi in phirange]
+export make_xsphere, make_ysphere, make_zsphere
+
+
 xsphere = [cos(phi) * sin(theta) for theta in thetarange, phi in phirange]
 ysphere = [sin(phi) * sin(theta) for theta in thetarange, phi in phirange]
 zsphere = [cos(theta) for theta in thetarange, phi in phirange]
